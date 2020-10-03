9dd35082edcb3e3c436c831405df8ce4f2f954bc
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-10-03 12:31:03 -0500

    importing pages into app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Splash from "./pages/Splash";
+import History from "./pages/History";
+import Inventory from "./pages/Inventory";
+import Roast from "./pages/Roast";
+import Social from "./pages/Social";
+import "./assets/styles/style.css";
+
 
 function App() {
   return (
